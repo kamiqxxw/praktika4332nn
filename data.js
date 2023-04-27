702c1adce3223fde05de1ebae6b862d9c0745ba1
kamiqxxw <126791524+kamiqxxw@users.noreply.github.com>
2023-04-27 18:18:01 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+// data.js
+export const context = {
+    languages: [
+      {
+        name: 'HTML',
+        modern: true,
+      },
+      {
+        name: 'CSS',
+        modern: true,
+      },
+      {
+        name: 'JavaScript',
+        modern: true,
+      },
+    ],
+  };
+  
